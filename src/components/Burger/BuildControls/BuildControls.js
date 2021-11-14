36dee010945ff305e2676075af7e0e1501a5c489
VijayHYadav <vizzyadav1997@gmail.com>
2021-11-14 15:22:58 +0530

    122 Connecting State to Build Controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,8 +12,8 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
-        {controls.map(ctr => (
-            <BuildControl key={ctr.label} label={ctr.label} />
+        {controls.map(ctrl => (
+            <BuildControl key={ctrl.label} label={ctrl.label} type={ctrl.type} added={() => props.ingredientAdded(ctrl.type)}/>
         ))}
     </div>
 );
--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -3,6 +3,13 @@ import React, { Component } from "react";
 import Auxi from "../hoc/Auxi";
 import Burger from "../components/Burger/Burger";
 import BuildControls from "../components/Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    bacon: 0.4,
+    cheese: 1.3,
+    meat: 0.7
+}
 class BurgerBuilder extends Component {
 
     // old school
@@ -18,14 +25,36 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updateCount = oldCount + 1;
+
+        // State should update in immutable way.
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+
+        updatedIngredients[type] = updateCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+    }
+
+    removeIngredientHandler = (type) => {
+
     }
 
     render() {
         return (
             <Auxi>
                 <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
+                <BuildControls ingredientAdded={this.addIngredientHandler}/>
             </Auxi>
         );
     }
