024d1673c70d5cd6337d816536a8d1acb54bcb83
VijayHYadav <vizzyadav1997@gmail.com>
2021-11-15 06:00:20 +0530

    143 Improving Performance.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Auxi from "../../../hoc/Auxi";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+
+    // This could be a functional component, doesn't have to be class
+    componentWillUpdate() {
+        console.log("[OrderSummary] willUpdate");
+    }
+
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}>
                     <span style={{textTransform: 'capitalize'}}>
                         {igKey}
-                    </span>: {props.ingredients[igKey]} 
+                    </span>: {this.props.ingredients[igKey]} 
                 </li>)
         });
 
-    return (
-        <Auxi>
-            <h3>Your Order</h3>
-            <p>A deliciour burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: ${props.price}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelHandler}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinueHandler}>CONTINUE</Button>
-        </Auxi>
-    );
-};
+        return (
+            <Auxi>
+                <h3>Your Order</h3>
+                <p>A deliciour burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: ${this.props.price}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelHandler}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinueHandler}>CONTINUE</Button>
+            </Auxi>
+        );
+    }
+}
 
-export default orderSummary;
+export default OrderSummary;
