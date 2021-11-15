17c7073c5ae03538da991661eb243289f6f80887
VijayHYadav <vizzyadav1997@gmail.com>
2021-11-15 08:58:01 +0530

    170 Sending a POST Request.

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     // 🔥 FireBase URL
-    baseURL: ' 🔥 FireBase URL '
+    baseURL: 'https://react-my-burger-55ff0.firebaseio.com/'
 });
 
 export default instance;
